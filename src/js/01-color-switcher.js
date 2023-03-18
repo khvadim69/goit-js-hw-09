d1260323d66b73f9b88d9e527bc2076f546bb230
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-18 12:08:16 +0200

    ше трохи поправив

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -18,9 +18,34 @@
 //   // console.log(`Interval with id ${timerId} has stopped!`);
 // });
 
+// const bodyEl = document.querySelector('body');
+// const startBtn = document.querySelector('button[data-start]');
+// const stopBtn = document.querySelector('button[data-stop]');
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// startBtn.addEventListener('click', addStylesBody);
+// let timerId = null;
+// function addStylesBody() {
+//   timerId = setInterval(() => {
+//     bodyEl.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+//   if (timerId) {
+//     startBtn.disabled = true;
+//     startBtn.removeEventListener('click', addStylesBody);
+//   }
+// }
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(timerId);
+//   startBtn.addEventListener('click', addStylesBody);
+//   startBtn.disabled = false;
+// });
+
+
 const bodyEl = document.querySelector('body');
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -40,5 +65,3 @@ stopBtn.addEventListener('click', () => {
   startBtn.addEventListener('click', addStylesBody);
   startBtn.disabled = false;
 });
-
-
