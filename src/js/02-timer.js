dd4d77b9a22b6dfd0505d596b18ae33e406fe80b
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-19 21:49:17 +0200

    2 працює

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,42 +4,61 @@ const datetimeEl = document.querySelector('#datetime-picker');
 console.log(datetimeEl);
 const startEl = document.querySelector('button[data-start]');
 console.log(startEl);
-const daysEl = document.querySelector("span[data-days]");
-console.log(daysEl)
-const hoursEl = document.querySelector("span[data-hours]");
-console.log(hoursEl)
-const minutesEl = document.querySelector("span[data-minutes]");
-console.log(minutesEl)
-const secondsEl = document.querySelector("span[data-seconds]");
-console.log(secondsEl)
-flatpickr(datetimeEl, {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    let selectDate = null;
-    if (selectedDates[0] <= new Date()) {
-      window.alert('Please choose a date in the future');
-      startEl.disabled = true;
-      selectDate = null;
-    } else {
-      startEl.disabled = false;
-      selectDate = selectedDates[0];
-      console.log(selectDate);
-    }
-    setInterval(() => {
-      if ((this.defaultDate = selectDate)) {
-        const defference = selectDate - new Date();
+const daysEl = document.querySelector('span[data-days]');
+console.log(daysEl);
+const hoursEl = document.querySelector('span[data-hours]');
+console.log(hoursEl);
+const minutesEl = document.querySelector('span[data-minutes]');
+console.log(minutesEl);
+const secondsEl = document.querySelector('span[data-seconds]');
+console.log(secondsEl);
+startEl.disabled = true;
+let selectDate = null;
+flatpickr(
+  datetimeEl,
+  (options = {
+    isActiv: false,
+    interval: null,
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if (selectedDates[0] <= new Date()) {
+        window.alert('Please choose a date in the future');
+        startEl.disabled = true;
+        selectDate = null;
+      } else {
+        startEl.disabled = false;
+        selectDate = selectedDates[0];
+        console.log(selectDate);
+        return;
+      }
+    },
+
+    start() {
+      if (this.isActiv) {
+        return;
+      }
+      let defference = 0;
+      this.interval = setInterval(() => {
+        defference = selectDate - new Date();
+        if (defference < 0) {
+          return clearInterval(this.interval);
+         }
+        this.isActiv = true;
         const components = convertMs(defference);
         console.log(components);
         daysEl.textContent = components.days;
         hoursEl.textContent = components.hours;
         minutesEl.textContent = components.minutes;
         secondsEl.textContent = components.seconds;
-      }
-    }, 1000);
-  },
+      }, 1000);
+    },
+  })
+);
+startEl.addEventListener('click', () => {
+  options.start();
 });
 function pad(value) {
   return String(value).padStart(2, '0');
@@ -55,4 +74,3 @@ function convertMs(ms) {
   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
 }
-// 
